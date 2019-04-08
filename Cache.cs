9e51601afe69f7fdec6ec14994f659af9dae3bdd
Neonalig <codychook@gmail.com>
2019-04-08 15:15:16 +0800

    Added Installer and Uninstaller

---
+++ Cache.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace QuickBrake {
+	class Cache {
+		public static bool CacheModified = false;
+		public static string CacheDirectory = "";
+		public static string CacheFile = "";
+
+		public static void Start() {
+			CacheDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "QuickBrake");
+			CacheFile = CacheDirectory + "\\Default.ini";
+			if (!Directory.Exists(CacheDirectory)) { Directory.CreateDirectory(CacheDirectory); }
+		}
+
+		public static void Setup() {
+			if (IsPortable) {
+				Program.HandBrakeLocation = Directory.GetCurrentDirectory() + "\\HandBrakeCLI.exe";
+				Program.FileSaveLocation = "%f%";
+			} else {
+				string tempH;
+				string tempF;
+
+				GetCache(handbrake: out tempH, saveloc: out tempF);
+				Console.WriteLine("HandBrake (CLI) Executable: " + tempH);
+				Console.WriteLine("Save Location: " + tempF);
+				Console.Write("\nDo you want to change these settings? [y/n]: ");
+
+				if (NeoLib.Prompt() == true) {
+					Console.Clear();
+					Process();
+				}
+				Environment.Exit(0);
+			}
+		}
+
+		public static void Process() {
+			Console.WriteLine("Updating Cache in: " + CacheFile);
+			if (Program.HandBrakeLocation.IsEmptyOrNull() || Program.HandBrakeLocation == "Unset") { CacheCheckOne(); }
+			if (Program.FileSaveLocation.IsEmptyOrNull() || Program.FileSaveLocation == "Unset") { CacheCheckTwo(); }
+			if (CacheModified) { SaveCache(); }
+		}
+
+		public static void CacheCheckOne() {
+			Console.WriteLine("HandBrake location not set in cache...");
+			DialogResult result = DialogResult.None;
+			OpenFileDialog dialog = NeoLib.BrowseForFile(out result, title: "Please locate 'HandBrakeCLI.exe'", checkFile: true, checkPath: true, extension: "exe", filter: "HandBrakeCLI.exe|HandBrakeCLI.exe");
+			if ((result == DialogResult.OK || result == DialogResult.Yes)) {
+				Console.WriteLine("Got path: " + dialog.FileName);
+				Program.HandBrakeLocation = dialog.FileName;
+			} else {
+				Program.HandBrakeLocation = "Unset";
+			}
+			CacheModified = true;
+			return;
+		}
+
+		public static void CacheCheckTwo() {
+			Console.WriteLine("Default save location not set in cache...");
+			Console.Write("\nWould you like to save files in the same location as the input? [y/n]: ");
+			if (NeoLib.Prompt() == true) { Program.FileSaveLocation = "%f%"; } else { FixFile(); }
+			CacheModified = true;
+			return;
+		}
+
+		public static void FixFile() {
+			Console.WriteLine("Please input where you would like your files to be saved (ie c:/foo).");
+			Console.WriteLine("[Hint: %f% will be replaced with the file's location during runtime]");
+			string c = Console.ReadLine();
+			if (c.Contains(":") && (c.Contains("/") || c.Contains(@"\"))) { Program.FileSaveLocation = c; } else { FixFile(); }
+			return;
+		}
+
+		public static void SaveCache() {
+			File.WriteAllLines(CacheFile, new string[] { Program.HandBrakeLocation, Program.FileSaveLocation });
+			return;
+		}
+
+		public static bool Set { get { return File.Exists(CacheFile) ? (!File.ReadAllText(CacheFile).IsEmptyOrNull()) : false; } set { Process(); } }
+		public static bool Exists { get { return File.Exists(CacheFile); } }
+
+		public static void GetCache(out string handbrake, out string saveloc) {
+			if (IsPortable) {
+				handbrake = Directory.GetCurrentDirectory() + "\\HandBrakeCLI.exe";
+				saveloc = "%f%";
+				Program.HandBrakeLocation = handbrake;
+				Program.FileSaveLocation = saveloc;
+				return;
+			} else {
+				if (!Directory.Exists(CacheDirectory)) {
+					Directory.CreateDirectory(CacheDirectory);
+					Installer.FirstTimeSetup();
+				}
+				if (!File.Exists(CacheFile)) {
+					File.Create(CacheFile).Dispose();
+					File.WriteAllLines(CacheFile, new string[] { "Unset", "Unset" });
+					Installer.FirstTimeSetup();
+				}
+
+				handbrake = File.ReadAllLines(CacheFile)[0];
+				saveloc = File.ReadAllLines(CacheFile)[1];
+
+				Program.HandBrakeLocation = handbrake;
+				Program.FileSaveLocation = saveloc;
+				return;
+			}
+		}
+
+		public static void GetCache() {
+			string h;
+			string s;
+			GetCache(out h, out s);
+			return;
+		}
+
+		public static bool IsPortable {
+			get {
+				return File.Exists(Directory.GetCurrentDirectory() + "\\Portable.ini");
+			} set {
+				if (value) {
+					File.Create(Directory.GetCurrentDirectory() + "\\Portable.ini");
+				} else {
+					File.Delete(Directory.GetCurrentDirectory() + "\\Portable.ini");
+				}
+			}
+		}
+	}
+}
---
+++ Installer.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.IO;
+using Microsoft.Win32;
+using Microsoft.VisualBasic.FileIO;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System.Security.Permissions;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace QuickBrake {
+	class Installer {
+
+		public static int setupCount = 0;
+		public static string backupInstallFolder = "";
+		public static string installFolder = "";
+		
+		[PermissionSet(SecurityAction.LinkDemand, Name = "FullTrust")]
+		public static void FirstTimeSetup() {
+			if (setupCount > 0) { return; }
+			backupInstallFolder = Path.GetFullPath(SpecialDirectories.ProgramFiles + "\\StarflashStudios\\QuickBrake Compressor\\");
+
+			Console.Write("This program has not been set-up yet, do you wish to do so? [y/n]: ");
+			if (!NeoLib.Prompt()) {
+				Cache.GetCache();
+				Program.Main(Program.args);
+				return;
+			}
+
+			Console.WriteLine("Initialising first time set-up...");
+			NeoLib.Wait(1000);
+			Console.Clear();
+
+			try { Directory.CreateDirectory(backupInstallFolder); } catch { InvalidAuthorisation(); return; }
+			if (!NeoLib.IsElevated) { InvalidAuthorisation(); return; }
+			Console.Write("Install to: " + backupInstallFolder + "? [y/n]: ");
+			if (NeoLib.Prompt()) {
+				installFolder = backupInstallFolder;
+				Install();
+			} else {
+				GetInstallDirectory();
+			}
+		}
+
+		public static void Install(bool auth = true) {
+			Console.WriteLine("Attempting Install...");
+			if (!Directory.Exists(installFolder)) { Directory.CreateDirectory(installFolder); }
+			if (auth) { InstallRegistry(); }
+			if (File.Exists(installFolder + "QuickBrake.exe")) { File.Delete(installFolder + "QuickBrake.exe"); }
+			File.Copy(Application.ExecutablePath, installFolder + "QuickBrake.exe");
+			Console.WriteLine("Please download the HandBrakeCLI.exe file from here: https://handbrake.fr/downloads2.php , and place into the installation folder here: " + installFolder);
+			NeoLib.Wait(5000);
+			Process.Start("https://handbrake.fr/downloads2.php");
+			Process.Start(installFolder);
+			Console.Write("\n\nPlease press enter once complete");
+			NeoLib.Prompt();
+			Console.Clear();
+			Cache.Process();
+			Console.WriteLine("Installation has finished, have a nice day :)");
+			NeoLib.Wait(2000);
+			Environment.Exit(0);
+		}
+
+		public static void Uninstall() {
+			Console.Clear();
+			Console.WriteLine("Very well... I can see that i am not wanted here *sniff*");
+			try { Registry.ClassesRoot.DeleteSubKeyTree("StarflashStudios.QuickBrakeCompressor.1"); } catch {
+				Console.WriteLine("\nAttempted to remove registry keys, but was unable");
+				Console.WriteLine("If this program was installed without elevated privileges, ignore this.");
+				Console.WriteLine("Otherwise, you may have to manually delete the SubKey 'StarflashStudios.QuickBrakeCompressor.1' and it's children found in 'Computer\\HKEY_CLASSES_ROOT'.");
+			}
+
+			try {
+				if (File.Exists(Program.HandBrakeLocation)) {
+					Console.Write("\nDelete: " + Program.HandBrakeLocation + " [y/n] ");
+					if (NeoLib.Prompt()) { File.Delete(Program.HandBrakeLocation); }
+				} else if (File.Exists(Directory.GetCurrentDirectory() + "\\HandBrakeCLI.exe")) {
+					Console.Write("\nDelete: " + Directory.GetCurrentDirectory() + "\\HandBrakeCLI.exe" + " [y/n] ");
+					if (NeoLib.Prompt()) { File.Delete(Directory.GetCurrentDirectory() + "\\HandBrakeCLI.exe"); }
+				}
+			} catch { } //HandBrakeCLI.exe already removed
+
+			try {
+				Console.Write("\nDelete cache? [y/n]: ");
+				if (NeoLib.Prompt()) { Directory.Delete(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "QuickBrake")); }
+			} catch { } //Cache already removed
+
+			if (File.Exists(Directory.GetCurrentDirectory() + "\\Portable.ini")) { File.Delete(Directory.GetCurrentDirectory() + "\\Portable.ini"); }
+			Console.WriteLine("We are sorry to see you go...");
+			NeoLib.Wait(1000);
+
+			Process.Start("cmd.exe", "/C choice /C Y /N /D Y /T 1 & Del \"" + Application.ExecutablePath + "\"", null, null, null);
+			Environment.Exit(0);
+		}
+
+		public static void InvalidAuthorisation() {
+			Console.Clear();
+			Console.Write("User has insufficient authorisation.\nPick a custom installation location [y]; or install in portable mode [n]: ");
+			if (NeoLib.Prompt()) {
+				GetInstallDirectory(false);
+			} else {
+				File.Create(Directory.GetCurrentDirectory() + "\\Portable.ini").Dispose();
+				Console.WriteLine("Installed in portable mode...");
+				NeoLib.Wait(1000);
+				Environment.Exit(1);
+			}
+		}
+		
+		public static void GetInstallDirectory(bool auth = true, bool cfd = true) {
+			Console.Clear();
+			if (cfd) {
+				CommonOpenFileDialog dialog = new CommonOpenFileDialog {
+					Title = "Pick the program's Installation location",
+					IsFolderPicker = true,
+					Multiselect = false,
+					EnsurePathExists = true,
+					EnsureFileExists = false,
+					AllowNonFileSystemItems = false,
+				};
+				if (auth) {
+					dialog.InitialDirectory = SpecialDirectories.ProgramFiles + "\\StarflashStudios\\QuickBrake Compressor\\";
+				} else {
+					dialog.InitialDirectory = SpecialDirectories.MyDocuments;
+				}
+				CommonFileDialogResult result = dialog.ShowDialog();
+				if (result == CommonFileDialogResult.Ok) {
+					Console.Write("Got: " + dialog.FileName + "\\ -- is this correct? [y/n]: ");
+					if (NeoLib.Prompt()) {
+						Console.WriteLine("Continuing...");
+						installFolder = dialog.FileName + "\\";
+						Install(auth);
+					} else {
+						GetInstallDirectory(auth, cfd);
+					}
+				} else if (result == CommonFileDialogResult.Cancel) {
+					CancelInstall();
+				} else {
+					GetInstallDirectory(auth, cfd);
+				}
+			} else {
+				FolderBrowserDialog browse = new FolderBrowserDialog();
+				DialogResult result = browse.ShowDialog();
+				switch (result) {
+					case DialogResult.OK:
+						installFolder = browse.SelectedPath;
+						break;
+					case DialogResult.Yes:
+						installFolder = browse.SelectedPath;
+						break;
+					case DialogResult.Cancel:
+						CancelInstall();
+						break;
+					case DialogResult.Abort:
+						CancelInstall();
+						break;
+					default:
+						GetInstallDirectory(auth, cfd);
+						break;
+				}
+			}			
+		}
+
+		public static void CancelInstall() {
+			Console.WriteLine("Installation process cancelled...");
+			NeoLib.Wait(2000);
+			Environment.Exit(0);
+		}
+
+		public static void InstallRegistry() {
+			RegistryKey BaseKey = Registry.ClassesRoot.CreateSubKey("StarflashStudios.QuickBrakeCompressor.1");
+			BaseKey.SetValue("", "QuickBrake Video Compressor");
+			BaseKey.SetValue("FriendlyTypeName", "@QuickBrake, -120");
+			RegistryKey VerKey = BaseKey.CreateSubKey("CurVer");
+			VerKey.SetValue("", "StarflashStudios.QuickBrakeCompressor.1");
+			VerKey.Close();
+			RegistryKey IconKey = BaseKey.CreateSubKey("DefaultIcon");
+			IconKey.SetValue("", "QuickBrake, 0");
+			IconKey.Close();
+			RegistryKey Shellkey = BaseKey.CreateSubKey("shell");
+			RegistryKey PlayKey = Shellkey.CreateSubKey("play");
+			RegistryKey CommandKey = PlayKey.CreateSubKey("command");
+			CommandKey.SetValue("", "\"%ProgramFiles%\\StarflashStudios\\QuickBrake Compressor\\Quickbrake.exe\" \"%1\"");
+			CommandKey.Close();
+			PlayKey.Close();
+			Shellkey.Close();
+			BaseKey.Close();
+		}
+	}
+}
